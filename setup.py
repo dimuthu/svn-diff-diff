876e2c7a8e3cba49d4015ac8571a1f321b74f3bc
unknown <d.nilanka@STD-SRI-DT-003.corp.ism.nl>
2014-11-10 13:22:51 +0530

    add setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+buildOptions = dict(packages = [], excludes = [])
+
+import sys
+base = 'Win32GUI' if sys.platform=='win32' else None
+
+executables = [
+    Executable('main.py', base=base)
+]
+
+setup(name='SVN Diff Diff',
+      version = '1.0',
+      description = 'A small tool to export file structure of a SVN diff of two revisions.',
+      options = dict(build_exe = buildOptions),
+      executables = executables)
